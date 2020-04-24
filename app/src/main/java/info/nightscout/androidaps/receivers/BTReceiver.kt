7d87ffd0640e4694509aedc38ee46d0a9e47a807
Andy Rozman <andy.rozman@gmail.com>
2020-04-24 17:40:19 +0100

    - add log

--- app/src/main/java/info/nightscout/androidaps/receivers/BTReceiver.kt
+++ app/src/main/java/info/nightscout/androidaps/receivers/BTReceiver.kt
@@ -5,15 +5,20 @@ import android.content.Context
 import android.content.Intent
 import dagger.android.DaggerBroadcastReceiver
 import info.nightscout.androidaps.events.EventBTChange
+import info.nightscout.androidaps.logging.AAPSLogger
+import info.nightscout.androidaps.logging.LTag
 import info.nightscout.androidaps.plugins.bus.RxBusWrapper
 import javax.inject.Inject
 
 class BTReceiver : DaggerBroadcastReceiver() {
     @Inject lateinit var rxBus: RxBusWrapper
+    @Inject lateinit var aapsLogger: AAPSLogger
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
         val device : BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+
+        aapsLogger.debug(LTag.CORE,  "BT Device: " + device)
 
         when (intent.action) {
             BluetoothDevice.ACTION_ACL_CONNECTED ->
